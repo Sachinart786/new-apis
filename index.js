285ad806162a8b060d938e5064f083940e0f118f
Sachinart786 <sachinart4400@gmail.com>
2025-02-17 17:02:22 +0530

    ecom

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ require("dotenv").config();
 const port = process.env.PORT || 8000;
 
 const productRouter = require("./routes/product");
+const productsRouter = require("./routes/products");
 const loginRouter = require("./routes/auth");
 const billRouter = require("./routes/sell");
 const stockRouter = require("./routes/stock");
@@ -31,6 +32,7 @@ app.use(express.json());
 
 app.use("/api/v1/bill", billRouter);
 app.use("/api/v1/product", productRouter);
+app.use("/api/v1/products", productsRouter);
 app.use("/api/v1/stock", stockRouter);
 app.use("/api/v1/auth", loginRouter);
 
