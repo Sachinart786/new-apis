acc7b91c1bd4bbdcf63c15641d8cb5bd05ed6d33
Sachinart786 <sachinart4400@gmail.com>
2025-02-24 13:11:09 +0530

    search

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,112 +1,4 @@
-// const Album = require("../models/albums");
-
-// const handleAddAlbums = async (req, res) => {
-//   const { id, title, tracks, music, lyric, year, playingTime, totalSize } =
-//     req.body;
-
-//     console.log("tracks", tracks);
-
-//   if (
-//     !title ||
-//     !Array.isArray(tracks) ||
-//     !music ||
-//     !lyric ||
-//     !year ||
-//     !playingTime ||
-//     !totalSize
-//   ) {
-//     return res.status(400).send({
-//       message:
-//         "Invalid input: title, tracks (array), music, lyric, year, playingTime, and totalSize are required.",
-//       success: false,
-//     });
-//   }
-
-//   if (
-//     tracks.length === 0 ||
-//     !tracks.every(
-//       (track) => track.no && track.title && track.singers && track.length
-//     )
-//   ) {
-//     return res.status(400).send({
-//       message:
-//         "Invalid track details: Each track must have 'no', 'title', 'singers', and 'length'.",
-//       success: false,
-//     });
-//   }
-
-//   try {
-//     const existingAlbum = await Album.findOne({
-//       title: { $regex: new RegExp(`^${title}$`, "i") },
-//     });
-
-//     if (existingAlbum) {
-//       return res.status(409).send({
-//         message: "Album already exists.",
-//         success: false,
-//       });
-//     } else {
-//       const newAlbum = new Album({
-//         id,
-//         title,
-//         tracks,
-//         music,
-//         lyric,
-//         year,
-//         playingTime,
-//         totalSize,
-//       });
-
-//       await newAlbum.save();
-
-//       return res.status(201).send({
-//         message: "Album added successfully",
-//         success: true,
-//       });
-//     }
-//   } catch (error) {
-//     console.error("Error in handleAddAlbums:", error);
-//     return res.status(500).send({
-//       message: "Internal Server Error",
-//       success: false,
-//     });
-//   }
-// };
-
-// const handleGetAllAlbums = async (req, res) => {
-//   try {
-//     const tasks = await Album.find({});
-//     res.status(200).send({ data: tasks, success: true });
-//   } catch (error) {
-//     res.status(500).send("Internal Server Error");
-//   }
-// };
-
-// module.exports = { handleGetAllAlbums, handleAddAlbums };
-
 const Album = require("../models/albums");
-const multer = require("multer");
-const path = require("path");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype === "image/png") {
-      cb(null, true);
-    } else {
-      cb(new Error("Only PNG images are allowed"), false);
-    }
-  },
-});
 
 const handleAddAlbums = async (req, res) => {
   const { title, tracks, music, lyric, year, playingTime, totalSize } =
@@ -164,10 +56,9 @@ const handleAddAlbums = async (req, res) => {
         year,
         playingTime,
         totalSize,
-        image: image || "", // Save image filename if present, else an empty string
+        image: image || "",
       });
 
-      // Save the new album to the database
       await newAlbum.save();
 
       return res.status(201).send({
@@ -234,9 +125,35 @@ const handleGetAlbum = async (req, res) => {
   }
 };
 
+const handleSearchAlbum = async (req, res) => {
+  const { name } = req.params;
+  try {
+    const album = await Album.find({
+      title: { $regex: name, $options: "i" },
+    });
+
+    if (!album) {
+      return res.status(404).send({
+        message: "Album not found",
+        success: false,
+      });
+    }
+    res.status(200).send({
+      data: album,
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error fetching album:", error);
+    res.status(500).send({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
+
 module.exports = {
   handleAddAlbums,
   handleGetAllAlbums,
   handleGetAlbum,
-  upload,
+  handleSearchAlbum,
 };
--- routes/albums.js
+++ routes/albums.js
@@ -4,9 +4,11 @@ const {
   handleAddAlbums,
   handleGetAllAlbums,
   handleGetAlbum,
+  handleSearchAlbum
 } = require("../controllers/albums");
 
 router.route("/").post(handleAddAlbums).get(handleGetAllAlbums);
 router.route("/:id").get(handleGetAlbum);
+router.route("/search/:name").get(handleSearchAlbum);
 
 module.exports = router;
