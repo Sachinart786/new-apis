a1085cf9770c62c0b45dfad27eb00c05386489f1
Sachinart786 <sachinart4400@gmail.com>
2025-02-27 16:27:11 +0530

    u

--- controllers/albums.js
+++ controllers/albums.js
@@ -214,12 +214,14 @@ const handleMail = async (req, res) => {
 
     return res.status(200).json({
       message: 'Email sent successfully',
-      info, // Include info about the email (like accepted recipients)
+      data: info,
+      success: false,
     });
 
   } catch (error) {
     console.error('Error sending email:', error);
     return res.status(500).json({
+      success: false,
       message: 'Error sending email',
       error: error.message, // Provide the specific error message
     });
